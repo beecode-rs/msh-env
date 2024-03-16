c18c2e03328ca796545e4aa5a87b26c1aa83e5de
milosbugarinovic <milos.bugarinovic@gmail.com>
2024-03-16 09:46:49 +0100

    fix: jest to use esmodule

--- test/jest.config.ts
+++ test/jest.config.ts
@@ -3,16 +3,16 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 const jestConfig: JestConfigWithTsJest = {
 	maxConcurrency: 1,
 
-	moduleFileExtensions: ['js', 'ts'],
 	setupFilesAfterEnv: ['jest-extended/all'],
-	transform: {},
+	transform: {
+		'^.+\\.js$': 'babel-jest',
+	},
 	preset: 'ts-jest/presets/default-esm',
 	// roots: ['<rootDir>'],
 	clearMocks: true,
 	// preset: 'ts-jest',
 	testEnvironment: 'node',
 	testMatch: ['<rootDir>/**/*.(spec|test).[jt]s?(x)'],
-	extensionsToTreatAsEsm: ['.ts'],
 	testPathIgnorePatterns: ['/node_modules/'],
 }
 
